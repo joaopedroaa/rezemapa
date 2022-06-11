031d002c38ed16ac71927c6f82375b8109d34d06
João Pedro Alvarenga <joaopedroaats@pm.me>
2022-06-11 11:43:45 -0300

    add more lero-lero text

--- pages/index.js
+++ pages/index.js
@@ -59,13 +59,75 @@ export default function Home() {
           </div>
         </div>
 
+        <div className={`${styles.download} ${styles.download2}`}>
+          <h3>Por que baixar esse mapa agora mesmo?</h3>
+          <p>
+            Com o nosso mapa a prática cotidiana prova que a consulta aos
+            diversos militantes faz parte de um processo de gerenciamento dos
+            conhecimentos estratégicos para atingir a excelência. Neste sentido,
+            a competitividade nas transações comerciais desafia a capacidade de
+            equalização dos relacionamentos verticais entre as hierarquias.
+          </p>
+
+          <p>
+            Todas estas questões, devidamente ponderadas, levantam dúvidas sobre
+            se a hegemonia do ambiente político exige a precisão e a definição
+            do remanejamento dos quadros funcionais. No mundo atual, a estrutura
+            atual da organização causa impacto indireto na reavaliação das
+            condições financeiras e administrativas exigidas.
+          </p>
+        </div>
+
+        <div className={`${styles.download} `}>
+          <h3>Com o este mapa, você será capaz de:</h3>
+          <ul>
+            <li>
+              A nível organizacional, o desenvolvimento contínuo de distintas
+              formas de atuação talvez venha a ressaltar a relatividade das
+              condições inegavelmente apropriadas.
+            </li>
+
+            <li>
+              É importante questionar o quanto o fenômeno da Internet pode nos
+              levar a considerar a reestruturação de alternativas às soluções
+              ortodoxas.
+            </li>
+            <li>
+              Podemos já vislumbrar o modo pelo qual o comprometimento entre as
+              equipes maximiza as possibilidades por conta dos modos de operação
+              convencionais.
+            </li>
+          </ul>
+        </div>
+
+        <div className={`${styles.download} ${styles.download2}`}>
+          <h3>Não se preocupe, você está totalmente seguro.</h3>
+          <p>
+            Todas estas questões, devidamente ponderadas, levantam dúvidas sobre
+            se a execução dos pontos do programa oferece uma interessante
+            oportunidade para verificação das posturas dos órgãos dirigentes com
+            relação às suas atribuições.
+          </p>
+          <p>
+            Assim mesmo, a consulta aos diversos militantes deve passar por
+            modificações independentemente dos níveis de motivação
+            departamental. A nível organizacional, a contínua expansão de nossa
+            atividade é uma das consequências do sistema de participação geral.
+          </p>
+          <p>
+            A certificação de metodologias que nos auxiliam a lidar com a
+            determinação clara de objetivos obstaculiza a apreciação da
+            importância do levantamento das variáveis envolvidas.
+          </p>
+        </div>
+
         <div className={styles.download}>
-          <span>Não perca esta oportunidade e baixe agora 100% grátis</span>
+          <h3>NÃO PERCA ESTA OPORTUNIDADE E BAIXE AGORA 100% GRATUITO</h3>
           <a
             href="https://github.com/joaopedroaa/rezemapa/raw/master/public/Etrr.rar"
             className={styles.buttonDownload}
           >
-            Baixe agora
+            SIM! EU QUERO BAIXAR AGORA!
           </a>
         </div>
 
